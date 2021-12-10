8d3fac7431f9da74839b7aa278e1a914c76faa1d
evgeny.klabukov <evgeny.klabukov@gmail.com>
2021-12-10 20:59:52 +0200

    add TextView

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.helloapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        float textSize = 22;
+        TextView textView = new TextView(this);
+        textView.setText("Hello Android!");
+        textView.setTextSize(textSize);
+        setContentView(textView);
     }
 }
